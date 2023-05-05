8073c3c47cb65fb86eb46a1399b0f989772dcfab
zedwong <kingflyme1@gmail.com>
2023-05-05 10:13:04 +0900

    add examples

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import ccxt
 import time
+import tomli
 import random
 
-API_KEY = 'api_key'  # Enter api_key
-SECRET = 'secret'  # Enter secret
-PASSPHRASE = 'passphrase'  # Enter passphrase
+with open("config.toml", mode="rb") as fp:
+    config = tomli.load(fp)
 
-TOKEN = 'USDC'  # Enter token => 'ETH', 'BNB', 'AVAX', 'USDC', 'USDT', etc.
-NETWORK = 'Polygon'  # Enter network => 'Arbitrum one', 'Polygon', 'ERC20', 'zkSync Lite', 'Optimism', 'BSC', 'TRC20', 'Avalanche C-Chain', etc.
-AMOUNT = round(random.uniform(1.0, 1.5), 6)  # 10 - min amount, 20 - max amount, 6 - digits after . (eg. 10.647293) => enter your min and max
+API_KEY=config["API_KEY"]
+SECRET=config["SECRET"]
+PASSPHRASE=config["PASSPHRASE"]
+
+TOKEN=config['TOKEN']
+NETWORK=config['NETWORK']
+AMOUNT_MIN=config['AMOUNT_MIN']
+AMOUNT_MAX=config['AMOUNT_MAX']
+
+AMOUNT = round(random.uniform(AMOUNT_MIN, AMOUNT_MAX), 6)  # 10 - min amount, 20 - max amount, 6 - digits after . (eg. 10.647293) => enter your min and max
 
 MIN_DELAY = 20  # Min seconds for delay between withdraws
 MAX_DELAY = 50  # Max seconds for delay between withdraws
@@ -38,6 +45,7 @@ def withdraw_info(value, wallet, token, amount, network):
 
 def withdraw(token, network, amount, wallet):
     network_fee = token_fee(token, network)
+    print("network_fee:", network_fee)
     try:
         exchange.withdraw(token, amount, wallet, params={
             'toAddress': wallet,
@@ -50,8 +58,9 @@ def withdraw(token, network, amount, wallet):
         })
 
         print(withdraw_info('success', wallet, token, amount, network))
-    except Exception:
+    except Exception as e:
         print(withdraw_info('error', wallet, token, amount, network))
+        print("Error:", e)
 
 
 def main():
@@ -64,4 +73,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    print('loaded:',config)
+    #main()
